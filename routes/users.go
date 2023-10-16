7a86784a43f1589358030c18b72902bf9ffe9cc6
Adrian <A01198211@tec.mx>
2023-10-15 18:21:17 -0600

    add tags

--- routes/users.go
+++ routes/users.go
@@ -5,8 +5,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func setUserRoutes(app *fiber.App){
-    app.Get("/users" , controllers.GetAllUsers)
-    app.Post("/users", controllers.AddUser)
-    app.Post("/users/addFavorites" , controllers.AddtoFavorites)
+func setUserRoutes(app *fiber.App) {
+	app.Get("/users", controllers.GetAllUsers)
+	app.Post("/users", controllers.AddUser)
+	app.Post("/users/addFavorites", controllers.AddtoFavorites)
+	app.Post("/users/addTags", controllers.AddTags)
 }
