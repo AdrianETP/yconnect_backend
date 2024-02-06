2a9434052301929049f3da53c83ba1d3b4429140
adrian <eduardotrevinop@yahoo.com.mx>
2024-02-05 19:48:34 -0600

    added routes of forum comments

--- routes/forums.go
+++ routes/forums.go
@@ -9,4 +9,7 @@ func SetForumRoutes(app *fiber.App) {
 	app.Post("/forums", controllers.AddForum)
 	app.Post("/forums/GetFromOrg", controllers.GetForumsFromOrg)
 	app.Post("/forums/GetFromUser", controllers.GetForumsByUser)
+	app.Post("/forums/Comments", controllers.AddForumComment)
+	app.Post("/forums/Comments/GetCommmentsFromPost", controllers.GetCommentsFromForum)
+	app.Post("/forums/Comments/Like", controllers.LikeForumComment)
 }
