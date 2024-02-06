867e7821985345dd9ea0e24279c7a3aff92034c4
adrian <eduardotrevinop@yahoo.com.mx>
2024-02-05 18:38:24 -0600

    likes de foros

--- models/forums.go
+++ models/forums.go
@@ -3,9 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Forums struct {
-	Id        primitive.ObjectID `bson:"_id, omniempty" json:id`
-	UserId    primitive.ObjectID `bson:"userId, omniempty" json:userId`
-	OrgId     primitive.ObjectID `bson:"orgId, omniempty" json:orgId`
-	Content   string             `bson:"content, omniempty" json:content`
-	TimeStamp primitive.DateTime `bson:"timestamp, omniempty" json:timestamp`
+	Id        primitive.ObjectID   `bson:"_id, omniempty" json:id`
+	UserId    primitive.ObjectID   `bson:"userId, omniempty" json:userId`
+	OrgId     primitive.ObjectID   `bson:"orgId, omniempty" json:orgId`
+	Content   string               `bson:"content, omniempty" json:content`
+	TimeStamp primitive.DateTime   `bson:"timestamp, omniempty" json:timestamp`
+	Likes     []primitive.ObjectID `bson:"likes, omniempty" json:likes`
 }
